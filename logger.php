b8d45b477b6b147eebeb1e4bdc477d35af1a1c55
ginandra <31370524+ginandra@users.noreply.github.com>
2025-08-17 04:58:52 +0800

    Update logger.php

--- logger.php
+++ logger.php
@@ -1,11 +1,15 @@
 <?php
+// File log
 $logFile = __DIR__ . "/visitor.log";
 
-$ip        = $_SERVER['REMOTE_ADDR'];
-$userAgent = $_SERVER['HTTP_USER_AGENT'];
-$time      = date("Y-m-d H:i:s");
+// Data pengunjung
+$ip       = $_SERVER['REMOTE_ADDR'];
+$userAgent= $_SERVER['HTTP_USER_AGENT'];
+$time     = date("Y-m-d H:i:s");
 
-$log = "[$time] IP: $ip | User-Agent: $userAgent" . PHP_EOL;
+// Isi log
+$log = "[$time] IP: $ip | User-Agent: $userAgent\n";
+
+// Simpan ke file (akan buat file baru kalau belum ada)
 file_put_contents($logFile, $log, FILE_APPEND);
-
-echo "ok";
+?>
